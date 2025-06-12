d40b812b6367cd75e99e73c6be339dfaf1b96c89
jordanbushey <jordanbushey83@gmail.com>
2025-06-12 14:59:05 -0400

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        //Create Player 1 - Ask user to input name
+
+        //Create Player 2
+
+        //Create Game Board
+
+        //Create Scoreboard
+
+        //Method to determine winner
+
+        //Method to determine tie
+
+
+    }
+}
