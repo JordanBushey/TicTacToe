7a627bbd5f6463d260fc03f92f13e0a878fd9ee6
jordanbushey <jordanbushey83@gmail.com>
2025-06-12 20:49:20 -0400

    Scanner added for user input

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,27 @@
+import java.util.Scanner; //Import Scanner Class for user input
+
 public class Main {
     public static void main(String[] args) {
 
+        //Create the Scanner Object
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter username:");
 
+        String userName = myObj.nextLine(); // Read User input using scanner object
+
+        System.out.println("Let's Play Tic Tac Toe!");
+        System.out.println("Game Board");
+        System.out.println("- - -");
+        System.out.println("- - -");
+        System.out.println("- - -");
         //Create Player 1 - Ask user to input name
 
-        //Create Player 2
+        //Create Player Builder Class
 
         //Create Game Board
+        // 9 objects
+
+        //Method to count turns
 
         //Create Scoreboard
 
